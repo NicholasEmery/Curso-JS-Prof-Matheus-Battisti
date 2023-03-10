5b0a2e00a86d4260cb8ed8eae5f3066f20a9a583
Nicholas Emery <108698936+NicholasEmery@users.noreply.github.com>
2023-03-10 17:48:15 -0300

    Adicionei as pastas

---
+++ 8_undefined_e_null/js/script.js
@@ -0,0 +1,12 @@
+// Hoisting - içamento
+console.log(sobrenome);
+
+var nome = null;
+var sobrenome = "Almeida";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Nicholas";
+
+console.log(nome);
---
+++ 9_objetos/js/script.js
@@ -0,0 +1,26 @@
+var obj = {
+    nome: "Nicholas",
+    idade: 16,
+    profissao: "Programador",
+    estaTrabalhando: true,
+};
+
+console.log(obj);
+
+console.log(typeof obj);
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
+
+console.log("O meu nome é " + obj.nome);
+
+obj.nome = "Matheus";
+
+console.log(obj.nome);
+
+console.log(obj);
+
+obj.graduacao = true;
+
+console.log(obj);
